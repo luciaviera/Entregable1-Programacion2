977598aa95e6893f82e719891b7c1fb7582889fb
Lucia Viera <luciaviera777@gmail.com>
2025-04-30 23:26:45 -0300

    Menu seleccion de jugadores validada

--- src/triangulos/interfaz/Menu.java
+++ src/triangulos/interfaz/Menu.java
@@ -63,14 +63,38 @@ public class Menu {
                            return;
                   } 
                   mostrarJugadores();
+                  Consola.println("\nSeleccion de jugadores: ");
+                  int indiceJ1 = leerIndice("Ingrece el numero correspondiente al primer jugador ");
+                   int indiceJ2 = leerIndice("Ingrece el numero correspondiente al segundo jugador ");
+                  Jugador jugador1 = jugadores.get(indiceJ1 - 1);
+                  while (indiceJ1 == indiceJ2) {
+                      Consola.error("No puede ser el mismo jugador. Elige otro.");
+                      indiceJ2 = leerIndice("Ingrece el numero correspondiente al segundo jugador ");
+                  }
+                  Jugador jugador2 = jugadores.get(indiceJ2 - 1);
          }
+         
+         
          
          public static void mostrarJugadores(){
                   Consola.println("\nJUGADORES REGISTRADOS");
                   for (int i = 0; i <jugadores.size(); i++) {
                             Consola.println(i+1 + ". " +jugadores.get(i).getNombre());
                   }
-
          }
+         
+        private static  int leerIndice(String prompt) {
+                while (true) {
+                    String line = Consola.readln(prompt);
+                    try {
+                        int val = Integer.parseInt(line);
+                        if (val >= 1 && val <= jugadores.size()) {
+                            return val;
+                        }
+                    } catch (NumberFormatException ignored) { }
+                    Consola.error("Por favor ingresa un número entre 1 y " + jugadores.size()+ ".");
+                }
+         }
+         
 
 }
